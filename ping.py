0cb631f9b4f04fd5dc582de17052539a65737e0b
♛Anonymous Haker♛ <95759540+Anonimus-Haker-Alon-Kreker-Darker@users.noreply.github.com>
2022-03-08 23:47:55 +0100

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,29 +1,44 @@
 import os, sys, time
-from os import system
-from time import sleep
+from os 
+import system
+from time
+import sleep
+from termcolor
+import colored
+class style():
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+    
 os.system("color A")
-os.system("inxi -c 4")
-print("~~" * 50)
-print("                                    Anonymous Alon kreker    ")
+print(style.BLACK + "~~" * 50)
+print(style.WHITE + "                                    Anonymous Alon kreker    ")
 time.sleep(50)
 os.system("cls")
 system("clear")
 print("~~" * 50)
-ip  = input ("Pingaj IP ili Domenu: ")
+ip  = input (style.BLACK + "Pingaj IP ili Domenu: ")
 os.system("ping " + ip)
 print("~~" * 50)
 time.sleep(50)
 os.system("cls")
 system("clear")
 print("~~" * 50)
-port  = input ("Nmap IP: ")
+port  = input (style.WHITE + "Nmap IP: ")
 os.system("nmap " + port)
 print("~~" * 50)
 time.sleep(50)
 os.system("cls")
 system("clear")
 print("~~" * 50)
-iph  = input ("IP Host: ")
+iph  = input (style.BLACK + "IP Host: ")
 os.system("host " + iph)
 print("~~" * 50)
 time.sleep(50)
